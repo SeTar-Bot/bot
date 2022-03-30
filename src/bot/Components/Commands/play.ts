e0234baa8d185674e8754f9a0fdde6af3d6e6f9e
EhsanFox <ehsan8184@gmail.com>
2022-03-30 16:39:23 +0430

    feat(play): Play Command beta 0.1

--- src/bot/Components/Commands/play.ts
+++ src/bot/Components/Commands/play.ts
@@ -6,6 +6,9 @@ import Command from "../../../Classes/Command";
 import { BotPermissions, localeList } from "../../../typings/enums";
 import { SoundCloud } from "music-engines";
 import { SupportedEngines } from "player-engine/dist/typings/Classes/Player";
+import { PlayerTrack } from "player-engine";
+import { APIInteractionGuildMember } from "discord-api-types/v9";
+import { VoiceConnection } from "@discordjs/voice";
 
 const basicInfo = {
     name: 'play',
@@ -42,6 +45,16 @@ const playCommand: Command = new Command({
         try {
             const input = ctx.options.getString('input', true);
             const engineChoice = ctx.options.getString('engine', false) as SupportedEngines ?? 'YouTube' as SupportedEngines;
+            const member = await ctx.guild.members.fetch({
+                user: ctx.user
+            });
+
+            // Handle User Errors
+            if(!member.voice?.channel)
+                await ctx.editReply(client.localeManager.getLocale(database.guild.locale as localeList).error.NoVoiceChannel().toOBJECT());
+
+            if(ctx.guild.me.voice?.channel && member.voice?.channel !== ctx.guild.me.voice?.channel)
+                await ctx.editReply(client.localeManager.getLocale(database.guild.locale as localeList).error.NoVoiceChannel().toOBJECT());
 
             const player = client.playerClient.createPlayer(ctx.guild, {
                 engine: engineChoice,
@@ -49,7 +62,15 @@ const playCommand: Command = new Command({
                 selfDeaf: true,
                 selfMute: false
             });
-            console.log(await player.search(input))
+            
+            const song: PlayerTrack = await player.search(input) as PlayerTrack;
+            const connection: VoiceConnection = player.connection(member.voice?.channel) as VoiceConnection;
+            
+            if(!connection)
+                await ctx.editReply(client.localeManager.getLocale(database.guild.locale as localeList).error.noContent().toOBJECT({ ephemeral: true }));
+            
+            await player.play([song], ctx);
+
             await ctx.editReply(client.localeManager.getLocale(database.guild.locale as localeList).reply.beta().toOBJECT());
         } catch (error) {
             throw error;
--- src/bot/Events/Ready.djs.ts
+++ src/bot/Events/Ready.djs.ts
@@ -18,7 +18,7 @@ const ReadyEvent: Event = new Event({
 
         try {
             const syncData = await client.database.sync(client.guilds.cache);
-            client.user.presence.set(botOptions.readyPresence(client));
+            client.user.setPresence(botOptions.readyPresence(client));
             myConsole.succeed(`Databse is now Synced | ${(!syncData.new || !syncData.old) ? `No Changes.`: `${chalk.bgBlue(`${syncData.new}`)} New Servers Added & ${chalk.bgRed(`${syncData.old}`)} Old Servers Removed`}`);
         } catch (error) {
             myConsole.fail(`Syncing database failed Due Error: ${error}`);
