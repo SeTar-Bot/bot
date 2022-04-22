acec83994d604d65dce26082a719bd0dda9a14ab
EhsanFox <EhsanFox@users.noreply.github.com>
2022-04-22 17:42:31 +0000

    Deploying to dev-prod from @ SeTar-Bot/bot@e2c043f9df0ba6a6f770f81cab03ea3358d14f2d 🚀

--- index.mjs
+++ index.mjs
@@ -3,8 +3,10 @@ import Classes from "./Classes/index.mjs";
 import SetarDB from "./Database/index.mjs";
 import { locales, localeManager } from "./locales/index.mjs";
 import * as BotEnums from "./typings/enums.mjs";
+const Bot = setarBot;
 const Locale = {
   list: locales,
   manager: localeManager
 };
+export default Bot;
 export { setarBot as Bot, Classes, SetarDB as Database, Locale, BotEnums };
