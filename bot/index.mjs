6254834e6697a60f6935126714e903fcac87eecf
EhsanFox <EhsanFox@users.noreply.github.com>
2022-05-11 09:55:07 +0000

    Deploying to dev-prod from @ SeTar-Bot/bot@90fd0d2cdf16a3d5beb4f0e64cc14e35ceb817a3 🚀

--- bot/index.mjs
+++ bot/index.mjs
@@ -1,6 +1,7 @@
 import Components from "./Components/index.mjs";
 import botOptions from "./Config/botOptions.mjs";
 import DebugEvent from "./Events/Debug.djs.mjs";
+import ErrorEvent from "./Events/Error.djs.mjs";
 import GuildCreateEvent from "./Events/GuildCreate.djs.mjs";
 import GuildDeleteEvent from "./Events/GuildDelete.djs.mjs";
 import InteractionEvent from "./Events/Interaction.djs.mjs";
