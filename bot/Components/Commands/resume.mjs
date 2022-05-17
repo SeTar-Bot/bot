279354604f12c671e0d392ed6b6aba4254be35f8
EhsanFox <EhsanFox@users.noreply.github.com>
2022-05-17 15:52:39 +0000

    Deploying to dev-prod from @ SeTar-Bot/bot@a6334e8bac28300a1a841431ec01f3484db96ff3 🚀

--- bot/Components/Commands/resume.mjs
+++ bot/Components/Commands/resume.mjs
@@ -17,7 +17,7 @@ const resumeCommand = new Command({ ...basicInfo,
     if (!member.voice?.channel) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.NoVoiceChannel().toOBJECT());
     if (ctx.guild.me.voice?.channel && member.voice?.channel !== ctx.guild.me.voice?.channel && ctx.guild.me.voice?.channel?.members?.size > 1) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.BotInUse().toOBJECT());
     if (!client.audioClient.client.connections.has(ctx.guild.id)) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.NothingPlaying().toOBJECT());
-    if (client.audioClient.client.connections.get(ctx.guild.id)?.dispatcher?.paused) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.player.AlreadyResumed().toOBJECT());
+    if (!client.audioClient.client.connections.get(ctx.guild.id)?.dispatcher?.paused) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.player.AlreadyResumed().toOBJECT());
     client.audioClient.resume(ctx.guild.id);
     return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).reply.player.resume().toOBJECT());
   }
