ce870d9d49ac83d66e2a5d05860c116874fcd60f
hesawmgh1098 <hesam.ghanbari1098@gmail.com>
2022-06-18 08:41:46 +0000

    Deploying to dev-prod from @ SeTar-Bot/bot@c8f78e77a56805a7f2fda64107cc7b086ee6bcdc 🚀

--- bot/Components/Buttons/musicPausePlay.mjs
+++ bot/Components/Buttons/musicPausePlay.mjs
@@ -6,19 +6,19 @@ const musicPausePlay = new Button({
   isAvailable: true,
   permission: BotPermissions.ALL,
   run: async (client, database, ctx) => {
-    const member = await ctx.guild.members.fetch({
+    const member = await ctx?.guild?.members.fetch({
       user: ctx.user
     }); // Handle User Errors
 
-    if (!member.voice?.channel) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.NoVoiceChannel().toOBJECT());
-    if (ctx.guild.me.voice?.channel && member.voice?.channel !== ctx.guild.me.voice?.channel && ctx.guild.me.voice?.channel?.members?.size > 1) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.BotInUse().toOBJECT());
-    if (!client.audioClient.client.connections.has(ctx.guild.id)) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.NothingPlaying().toOBJECT());
+    if (!member?.voice?.channel) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.NoVoiceChannel().toOBJECT());
+    if (ctx?.guild?.me?.voice?.channel && member.voice?.channel !== ctx.guild.me.voice?.channel && ctx.guild.me.voice?.channel?.members?.size > 1) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.BotInUse().toOBJECT());
+    if (!client.audioClient.client.connections.has(`${ctx?.guild?.id}`)) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.NothingPlaying().toOBJECT());
 
-    if (client.audioClient.client.connections.get(ctx.guild.id)?.dispatcher?.paused) {
-      client.audioClient.resume(ctx.guild.id);
+    if (client.audioClient.client.connections.get(`${ctx?.guild?.id}`)?.dispatcher?.paused) {
+      client.audioClient.resume(`${ctx?.guild?.id}`);
       return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).reply.player.resume().toOBJECT());
     } else {
-      client.audioClient.pause(ctx.guild.id);
+      client.audioClient.pause(`${ctx?.guild?.id}`);
       return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).reply.player.pause().toOBJECT());
     }
   }
