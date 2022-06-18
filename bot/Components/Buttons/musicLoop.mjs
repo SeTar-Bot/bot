615fe8cf7aa24ce623475d3bad1ba9abc35ace17
hesawmgh1098 <hesam.ghanbari1098@gmail.com>
2022-06-18 08:47:30 +0000

    Deploying to dev-prod from @ SeTar-Bot/bot@ee0cbce269a579e0f978431fbe7288bdfc7f353f 🚀

--- bot/Components/Buttons/musicLoop.mjs
+++ bot/Components/Buttons/musicLoop.mjs
@@ -6,20 +6,20 @@ const musicPausePlay = new Button({
   isAvailable: true,
   permission: BotPermissions.ALL,
   run: async (client, database, ctx) => {
-    const member = await ctx.guild.members.fetch({
+    const member = await ctx?.guild?.members.fetch({
       user: ctx.user
     }); // Handle User Errors
 
-    if (!member.voice?.channel) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.NoVoiceChannel().toOBJECT());
-    if (ctx.guild.me.voice?.channel && member.voice?.channel !== ctx.guild.me.voice?.channel && ctx.guild.me.voice?.channel?.members?.size > 1) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.BotInUse().toOBJECT());
-    if (!client.audioClient.client.connections.has(ctx.guild.id)) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.NothingPlaying().toOBJECT());
-    const queue = client.audioClient.getQueue(ctx.guild.id);
+    if (!member?.voice?.channel) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.NoVoiceChannel().toOBJECT());
+    if (ctx?.guild?.me?.voice?.channel && member.voice?.channel !== ctx.guild.me.voice?.channel && ctx.guild.me.voice?.channel?.members?.size > 1) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.BotInUse().toOBJECT());
+    if (!client.audioClient.client.connections.has(`${ctx?.guild?.id}`)) return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).error.NothingPlaying().toOBJECT());
+    const queue = client.audioClient.getQueue(`${ctx?.guild?.id}`);
 
-    if (queue.loopMode == "all" || queue.loopMode == "one") {
-      queue.setLoop("none");
+    if (queue?.loopMode == "all" || queue?.loopMode == "one") {
+      queue?.setLoop("none");
       return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).reply.player.loop("none").toOBJECT());
     } else {
-      queue.setLoop("all");
+      queue?.setLoop("all");
       return await ctx.editReply(client.localeManager.getLocale(database.guild.locale).reply.player.loop("all").toOBJECT());
     }
   }
